5b8cab849716b62272b15c811b9c601a31c0b3f8
Kelvide <kelvide7@gmail.com>
2022-02-28 21:53:16 +0100

    push todo-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { studentInterface } from './type/student.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public tasks:studentInterface[] = []
+  public taskName:string = "";
+  public obj? : object;
+  public editMode:boolean = false;
+  public editedIndex:number = 0;
   title = 'todo-list';
+
+  addTask() {
+    if (this.taskName == "") {
+      alert("Input Is Empty Please Fill it Up")
+    } else {
+      this.tasks.push(
+        this.obj = {
+          taskName : this.taskName
+        }
+      )
+      this.taskName = ""
+    }
+  }
+
+  delete(i:number){
+    this.tasks = this.tasks.filter((_, index) => index!=i)
+    alert("Task Deleted")
+  }
+
+  edit(i:number){
+    this.editMode = true;
+    this.editedIndex = i;
+    this.taskName = this.tasks[i].taskName
+  }
+  
+  save(){
+    if (this.taskName == ""){
+      alert("Input Is Empty Please Fill it Up")
+      this.editMode = true;
+    } else {
+      this.tasks[this.editedIndex] = {taskName:this.taskName};
+      this.taskName = "";
+      this.editMode = false;
+    }
+  }
 }
---
+++ src/app/type/student.interface.ts
@@ -0,0 +1,3 @@
+export interface studentInterface {
+    taskName: string
+}
